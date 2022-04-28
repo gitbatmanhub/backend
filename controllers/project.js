395695f752842e95e0f57a0b4cabc4c51eca3ba2
edwinromero <ecro98.98@gmail.com>
2022-04-28 11:35:42 -0500

    Método para guardar nuevos documentos

--- controllers/project.js
+++ controllers/project.js
@@ -1,17 +1,43 @@
 'use strict'
-
+var Project = require('../models/project');
 var controller = {
   home: function (req, res){
   return res.status(200).send({
     message:'Soy la home'
 });
   },
+
   test: function (req, res){
     return res.status(200).send({
       message:'Soy el metodo o acción test del controlador de project'
     });
+  },
 
+  saveProject: function (req, res){
+    var project = new Project();
+
+    var params= req.body;
+    project.name= params.name;
+    project.description= params.description;
+    project.category= params.category;
+    project.year= params.year;
+    project.langs= params.langs;
+    project.image= null;
+
+    project.save((err, projectStored)=>{
+      if(err) return res.status(500).send({message:'Error al guardar documento'});
+
+      if(!projectStored) return res.status(404).send({message:'No se ha podido guardar el projecto'});
+
+    })
+
+
+    return res.status(200).send({
+      project: project,
+      message: 'Método saveProject'
+    })
   }
+
 };
 
 
--- models/project.js
+++ models/project.js
@@ -9,7 +9,8 @@ var ProjectSchema= Schema({
     description: String,
     category: String,
     year: Number,
-    langs: [String]
+    langs: String,
+    image: String
 
 });
 
